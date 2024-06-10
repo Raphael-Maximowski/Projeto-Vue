15c249fd042fbcfa97069615e70d5a20b5f61231
Ane Gonçalves <goncavesanne961@gmail.com>
2024-06-10 16:37:38 -0300

    componentes navbar e cardshq

---
+++ src/components/NavBar.vue
@@ -0,0 +1,42 @@
+<template>
+    <nav class="navbar">
+        <div class="container-fluid">
+            <a><RouterLink to="/">Home</RouterLink></a>
+            <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="60" height="30" />
+            <form class="d-flex" role="search">
+            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+            <button class="btn btn-outline-danger" type="submit">Search</button>
+            </form>
+        </div>
+    </nav>
+
+</template>
+
+<script>
+
+
+export default {
+    name: 'NavBar',
+    data() {
+        
+    },
+}
+</script>
+<style scoped>
+.navbar {
+    background: #151515;
+    height: 80px;
+    width: 100%;
+}
+.container-fluid {
+    display: block;
+
+}
+a {
+    color : red;
+}
+
+button {
+    color: red;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
-import './assets/main.css'
-import 'bootstrap/dist/css/bootstrap.css'
-import bootstrap from 'bootstrap/dist/js/bootstrap.js'
+// import './assets/main.css'
+
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import 'bootstrap/dist/css/bootstrap.css'
+import bootstrap from 'bootstrap/dist/js/bootstrap.bundle.js'
 
 const app = createApp(App)
 
